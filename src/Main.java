6992be43cc500bd9323799fdd76eb8efe275cdbf
Elena Shobanova <eishobanova@ornitech.local>
2026-01-26 15:48:35 +0300

    Путь к файлу, счетчик верно указанных путей

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмине <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-
+        int countPath = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists && !isDirectory) {
+                countPath++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + countPath);
+                continue;
+            }
+            if (isDirectory)
+                System.out.println("Указанный путь является путём к папке, а не к файлу!");
+            else
+                System.out.println("Указанный файл не существует!");
+        }
     }
-}
+ }
+
