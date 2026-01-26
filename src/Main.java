79263224c46ec6cf429b2ac0c241ef4ac58f5b27
Elena Shobanova <eishobanova@ornitech.local>
2026-01-26 15:26:59 +0300

    Путь к файлу, счетчит верно указанных путей

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        int sum = firstNumber + secondNumber;
-        int dif = firstNumber - secondNumber;
-        int prod = firstNumber * secondNumber;
-        double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Сумма: " + sum);
-        System.out.println("Разность: " + dif);
-        System.out.println("Произведение: " + prod);
-        System.out.println("Частное: " + quotient);
-
+        int countPath = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists && !isDirectory) {
+                countPath++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + countPath);
+                continue;
+            }
+            if (isDirectory)
+                System.out.println("Указанный путь является путём к папке, а не к файлу");
+            else
+                System.out.println("Указанный файл не существует");
+        }
     }
-}
+ }
+
