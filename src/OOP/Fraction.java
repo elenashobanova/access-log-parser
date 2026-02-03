b69a7c208700a8ce59b7e805ffdc75e074a10c35
Elena Shobanova <eishobanova@ornitech.local>
2026-02-03 17:29:38 +0300

    ООП. Задача 8.5

--- src/OOP/Fraction.java
+++ src/OOP/Fraction.java
@@ -1,30 +1,34 @@
 package OOP;
 
-public class Fraction {
+public class Fraction extends Number {
     private final int numerator;
     private final int denominator;
 
     public Fraction(int numerator, int denominator) {
-        if (denominator < 0)
-            throw new IllegalArgumentException("Отрицательное значение недопустимо!");
+        if (denominator == 0)
+            throw new IllegalArgumentException("Здесь на ноль делить нельзя!");
         this.numerator = numerator;
         this.denominator = denominator;
     }
-    public Fraction sum(Fraction second){
-        int sumNum = this.numerator * second.denominator + this.denominator * second.numerator;
-        int sumDen = this.denominator * second.denominator;
-        return new Fraction(sumNum, sumDen);
+
+    @Override
+    public double doubleValue() {
+        return (double) numerator / denominator;
     }
-    public Fraction minus(Fraction second){
-        int sumNum = this.numerator * second.denominator - this.denominator * second.numerator;
-        int sumDen = this.denominator * second.denominator;
-        return new Fraction(sumNum, sumDen);
+
+    @Override
+    public int intValue() {
+        return (int) doubleValue();
     }
-    public Fraction sum(int x){
-        return sum(new Fraction(x, 1));
+
+    @Override
+    public long longValue() {
+        return (long) doubleValue();
     }
-    public Fraction minus(int x){
-        return minus(new Fraction(x, 1));
+
+    @Override
+    public float floatValue() {
+        return (float) doubleValue();
     }
 
     @Override
--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(1, 3);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(7, 8);
-        Fraction res = f1.sum(f2).sum(f3).minus(5);
-        System.out.println("f1: " + f1);
-        System.out.println("f2: " + f2);
-        System.out.println("f3: " + f3);
-        System.out.println(" f1.sum(f2).sum(f3).minus(5) = " + res);
+        Fraction f = new Fraction(6, 7);
+        System.out.println(f);
+        System.out.println("doudle: " + f.doubleValue());
+        System.out.println("int: " + f.intValue());
+        System.out.println("float: " + f.floatValue());
+        System.out.println("long: " + f.longValue());
+
     }
 }
